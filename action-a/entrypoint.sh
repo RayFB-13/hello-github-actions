eaea6c3ee6934909330fc2afc2bf06205f506863
RayFB-13 <74745745+RayFB-13@users.noreply.github.com>
2021-02-01 14:17:16 -0500

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,3 +2,5 @@
 
 sh -c "echo Hello  $INPUT_MY_NAME"
 sh -c "echo lolololololololol,  $INPUT_MY_NAME"
+sh -c "echo gogogo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sh -c "echo so lets,gogogogo!"
